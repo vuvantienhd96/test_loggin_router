dfe2d31519d57a92cd5c82830d06e572c88fac51
vuvantienhd96 <congtuocxauxa000@gmail.com>
2020-09-17 13:58:45 +0700

    Initial commit

---
+++ LoginPage/LoginPage.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+function LoginPage() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        password: ''
+    });
+    const [submitted, setSubmitted] = useState(false);
+    const { username, password } = inputs;
+    const loggingIn = useSelector(state => state.authentication.loggingIn);
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    //reset login status
+    useEffect(() => { 
+        dispatch(userActions.logout()); 
+    }, []);
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setInputs(inputs => ({ ...inputs, [name]: value }));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        setSubmitted(true);
+        if (username && password) {
+            // get return url from location state or default to home page
+            const { from } = location.state || { from: { pathname: "/" } };
+            dispatch(userActions.login(username, password, from));
+        }
+    }
+
+    return (
+        <div className="col-lg-8 offset-lg-2">
+            <h2>Login</h2>
+            
+        </div>
+    );
+}
+
+export { LoginPage };
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hooks-redux-registration-login-example-a5avo5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hooks-redux-registration-login-example-a5avo5)
